01c98496646115bac6db52becab3668f94228d51
Charles Ludwig <charleswludwig@gmail.com>
2023-06-18 14:38:02 -0400

    updating query display

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template, url_for, request
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('factbook.db')
+c = conn.cursor()
+
+def sql_execution(raw_query):
+    c.execute(raw_query)
+    data = c.fetchall()
+    # pandas
+    return data
 
 app = Flask(__name__)
 
@@ -9,8 +20,10 @@ def index():
 @app.route('/process_query', methods = ['GET', 'POST'])
 def process_query():
     if request.method== 'POST':
-        raw_query = request.form['raw_query']
-    return render_template('index.html', raw_query=raw_query)
+        raw_query = request.form["raw_query"]
+        results = sql_execution(raw_query)
+        results = pd.DataFrame(results)
+    return render_template('index.html', raw_query=raw_query, results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, use_reloader=True)
+    app.run(debug=True, port=5002, use_reloader=True)
