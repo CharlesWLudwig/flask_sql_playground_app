98b7bc5eb7e6b694e8f11fdc085225d45c506554
Charles Ludwig <charleswludwig@gmail.com>
2023-06-18 15:42:37 -0400

    updating app with dataframe support

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request
 import sqlite3
 import pandas as pd
 
-conn = sqlite3.connect('factbook.db')
+conn = sqlite3.connect('factbook.db', check_same_thread=False)
 c = conn.cursor()
 
 def sql_execution(raw_query):
@@ -15,7 +15,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    results = pd.DataFrame([])
+    return render_template('index.html', results=results)
 
 @app.route('/process_query', methods = ['GET', 'POST'])
 def process_query():
